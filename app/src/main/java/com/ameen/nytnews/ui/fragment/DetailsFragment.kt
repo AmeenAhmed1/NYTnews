c6d0a13029168c6906b7b7d12fee57fc3f19b168
AmeenAhmed1 <ameen.mobiledev@gmail.com>
2022-02-19 20:52:03 +0200

    Open Image in seperated Fragment with Sharing Image Link Button

--- app/src/main/java/com/ameen/nytnews/ui/fragment/DetailsFragment.kt
+++ app/src/main/java/com/ameen/nytnews/ui/fragment/DetailsFragment.kt
@@ -1,9 +1,12 @@
 package com.ameen.nytnews.ui.fragment
 
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.ameen.nytnews.R
 import com.ameen.nytnews.data.model.ArticleResult
 import com.ameen.nytnews.databinding.FragmentDetailsBinding
 import com.ameen.nytnews.util.loadImageUrl
@@ -31,9 +34,20 @@ class DetailsFragment : BaseFragment<FragmentDetailsBinding>() {
             dateText.text = selectedArticle.published_date
         }
 
+        binding.detailsImage.setOnClickListener { goToZoomFragment() }
+
         context?.loadImageUrl(
             selectedArticle.media?.firstOrNull()?.media_metadata?.lastOrNull()?.url,
             binding.detailsImage
         )
+    }
+
+    private fun goToZoomFragment() {
+        val bundle = Bundle()
+        bundle.putString(
+            "selectedImage",
+            selectedArticle.media?.firstOrNull()?.media_metadata?.lastOrNull()?.url
+        )
+        findNavController().navigate(R.id.action_detailsFragment_to_fullViewFragment, bundle)
     }
 }
